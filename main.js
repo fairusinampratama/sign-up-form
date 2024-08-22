e3245972aaa047e45d2b17f719113e9af3c1e92c
Fairus In'am Pratama <fairusinam08@gmail.com>
2024-08-22 09:50:13 +0700

    add everything

---
+++ main.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function(){
+    let password = document.querySelector("#password").value;
+    let confirmPassword = document.querySelector("#confirmPassword").value;
+
+    let error = document.querySelector("#error");
+
+    if (password != confirmPassword) {
+        error.innerHTML = "*Password do not matched"
+    }
+})
